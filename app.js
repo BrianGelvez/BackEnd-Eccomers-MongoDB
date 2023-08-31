6f07740c03674e4fa8e509c478ebda879d78ec5a
Brian Gelvez <briannn97@gmail.com>
2023-08-31 11:23:23 -0300

    a ver ahoraa

--- app.js
+++ app.js
@@ -102,7 +102,7 @@ console.log(upload);
     if(!files.length){
       return res.status(400).json({ err:'Please upload an image', msg:'Please upload an image' })
     }
-    let file = req.files[1]
+    let file = req.files[0]
     if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
         return res.json({"image" : file.filename}) 
     }
